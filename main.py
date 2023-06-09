5eff052eec241dd6ebb73df3c228582a96b0f2e9
Carissa Slone <133814003+sunnylikesunshine@users.noreply.github.com>
2023-06-09 17:08:48 -0400

    Found a test csv file for tweets. Modified program to handle this particular file.

--- main.py
+++ main.py
@@ -13,24 +13,27 @@ from SentimentCutOff import SentCutOff
 
 
 # read in csv file with annotated values for accuracy analysis
-tweets = pd.read_csv("twitter_training.csv")
+df = pd.read_csv("test.csv")
+
+# some of the sentiment cells are missing values. drop these from the data set
+df.dropna(subset=['sentiment'], inplace=True)
 
 # create new columns in df object to write calculated sentiment to
-tweets["vader"] = None
-tweets["textblob"] = None
-tweets["textblob number"] = None
-tweets["vader number"] = None
+df["vader"] = None
+df["textblob"] = None
+df["textblob number"] = None
+df["vader number"] = None
 
 # need instance of sentiment analyzer
 sia = SentimentIntensityAnalyzer()
 
 # preprocess text
-tweets["entry"] = tweets["entry"].apply(PreprocessText)
+df["text"] = df["text"].apply(PreprocessText)
 
 # iterate through text and collect a polarity score
 # using vader and textblob
 for index, row in df.iterrows():
-    entry = row['entry']
+    entry = row['text']
 
     # vader
     score_vader = sia.polarity_scores(entry) 
@@ -50,7 +53,7 @@ textblob_std = np.std(df['textblob number'])
 
 # use mean and std to determine cutoffs for POS/NEG/NEUT sentiment label
 for index, row in df.iterrows():
-    entry = row['entry']
+    entry = row['text']
 
     df.at[index, "vader"] = SentCutOff(vader_mean,vader_std,df.at[index,"vader number"]) # convert numerical score to POS/NEG/NEUT and write to "vader" column
 
@@ -58,12 +61,12 @@ for index, row in df.iterrows():
     df.at[index, "textblob"] = SentCutOff(textblob_mean,textblob_std,df.at[index,"textblob number"])
 
 
-# calculate classification report and confusion matrix for vader, textblob, chatgpt, and unwrap.ai results
+# calculate classification report and confusion matrix for vader and textblob results
 print("Compare Vader with annotated_sentiment.")
-print(classification_report(annot['annotated_sentiment'],df['vader']))
-print(confusion_matrix(annot['annotated_sentiment'], df['vader']))
+print(classification_report(df['sentiment'],df['vader']))
+print(confusion_matrix(df['sentiment'], df['vader']))
 
 
 print("Compare TextBlob with annotated_sentiment.")
-print(classification_report(annot['annotated_sentiment'],df['textblob']))
-print(confusion_matrix(annot['annotated_sentiment'], df['textblob']))
+print(classification_report(df['sentiment'],df['textblob']))
+print(confusion_matrix(df['sentiment'], df['textblob']))
